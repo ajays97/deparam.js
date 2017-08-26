4ecc42a164dfe3461afa8f7c160d5de361466b2c
Ajay Srinivas <ajay.s1997@gmail.com>
2017-08-26 08:06:40 +0530

    Add deparam.js

---
+++ deparam.js
@@ -0,0 +1,21 @@
+/**
+ * jQuery.deparam - The inverse of jQuery's $.param(). Takes a parameterized query string and converts into an object.
+ *
+ * Regex Credits: Steven Benner
+*/
+(function($){
+  $.deparam = $.deparam || function(uri){
+    if(uri === undefined){
+      uri = window.location.search;
+    }
+    var queryString = {};
+    uri.replace(
+      new RegExp(
+        "([^?=&]+)(=([^&#]*))?", "g"),
+        function($0, $1, $2, $3) {
+        	queryString[$1] = decodeURIComponent($3.replace(/\+/g, '%20'));
+        }
+      );
+      return queryString;
+    };
+})(jQuery);
